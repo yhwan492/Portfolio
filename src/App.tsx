68205c225170122b2511a6a2d332c31c05ab3ac3
yhwan492 <61440704+yhwan492@users.noreply.github.com>
2023-01-02 00:54:14 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header'
+import About from './components/About'
+import Nav from './components/Nav'
+import Skills from './components/Skills'
+import Experience from './components/Experience'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Skills />
+      <Experience />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
